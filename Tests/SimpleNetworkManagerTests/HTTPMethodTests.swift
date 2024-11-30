1f26db1ba660ecb17e2001907a00a2c7b924855d
Caleb Hermann <caleb.hermann@gmail.com>
2024-11-29 17:44:19 -0700

    Removed throws from test signature

--- Tests/SimpleNetworkManagerTests/HTTPMethodTests.swift
+++ Tests/SimpleNetworkManagerTests/HTTPMethodTests.swift
@@ -10,22 +10,22 @@ import Testing
 @testable import SimpleNetworkManager
 
 struct HTTPMethodTests {
-    @Test func testGetMethod() async throws {
+    @Test func testGetMethod() async {
         let method = HTTPMethod.get
         #expect(method.rawValue == "GET")
     }
     
-    @Test func testPostMethod() async throws {
+    @Test func testPostMethod() async {
         let method = HTTPMethod.post
         #expect(method.rawValue == "POST")
     }
     
-    @Test func testPutMethod() async throws {
+    @Test func testPutMethod() async {
         let method = HTTPMethod.put
         #expect(method.rawValue == "PUT")
     }
     
-    @Test func testDeleteMethod() async throws {
+    @Test func testDeleteMethod() async {
         let method = HTTPMethod.delete
         #expect(method.rawValue == "DELETE")
     }
